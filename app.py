62a3d5ba98a97d35da8a25f118208f33b18b4a78
Gamygams1234 <gamyburgos@gmail.com>
2020-06-13 18:38:38 -0700

    new years template

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import datetime
 
 
 # showing taht we are creating a new web application
@@ -7,10 +8,24 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    headline = "Hello everybody"
+    return render_template('index.html', headline=headline)
 
 
 @app.route("/<string:name>")
 def hello(name):
     name = name.capitalize()
     return f"Hello, {name}!"
+
+
+@app.route("/bye")
+def bye():
+    headline = "Bye everybody"
+    return render_template('index.html', headline=headline)
+
+
+@app.route("/newyear")
+def newyear():
+    now = datetime.datetime.now()
+    new_year = now.month == 1 and now.day == 1
+    return render_template('newyear.html', new_year=new_year)
